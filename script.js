197e1c38e81c51bb31e8148f86781f2389ca6389
Manav Lakhanpal <67679350+ML2712@users.noreply.github.com>
2023-03-15 00:38:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -69,3 +69,12 @@
 
     });
 })(jQuery);
+
+
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
